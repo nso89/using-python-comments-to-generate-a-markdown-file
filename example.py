87791e0a214d83b8cb4e0be66c67f6bc9ec19d49
nso89 <114881729+nso89@users.noreply.github.com>
2022-10-18 17:59:30 -0400

    Create example.py

---
+++ example.py
@@ -0,0 +1,10 @@
+def main():
+
+    # Using print():
+    print("Hello World") # Output: Hello World
+
+    # Using type():
+    print(type("Hello World")) # Output: <class 'str'>
+
+if __name__ == "__main__":
+    main()
