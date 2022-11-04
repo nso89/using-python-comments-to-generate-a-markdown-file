bc94a5953f751e0b1eb2f751ec70956fd94c76f4
nso89 <114881729+nso89@users.noreply.github.com>
2022-11-04 14:38:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 from typing import List
 from pathlib import Path
 
+def get_keywords_from(file_name : str) -> List[str]:
+    """
+    Read words from a file, strip and append to a 
+    list using a comprehension.
+
+    Args:
+        file_name: str
+    
+    Returns:
+        List[str]
+    """
+    with open(file_name, "r") as f_obj:
+        lines = [word.strip() for word in f_obj.readlines()]
+    return lines
+
 def write_to_file(file_name: str, items: List[str]) -> None:
     """
     Append a list to a file.
 
     Args:
-    file_name: str
-    items : List[str]
+        file_name: str
+        items : List[str]
 
     Returns:
-    None
+        None
     """
     with open(file_name, "a") as f_obj:
         for item in items:
@@ -22,40 +37,35 @@ def validate_parameters(validate: str, parameter: str) -> None:
     indicating the parameter cannot be blank.
 
     Args:
-    validate: str - the string we're checking.
-    parameter: str - what the str represents.
+        validate: str - the string we're checking.
+        parameter: str - what the str represents.
 
     Returns:
-    None
+        None
     """
     if not validate:
         raise ValueError(f"{parameter} cannot be blank!")
 
-def check_for_function_names(verify: str) -> str:
+def convert_keywords_to_markdown_syntax(keywords : set[str], verify: str) -> str:
     """
-    Using a for loop with str.split(), we check 
-    if verify ends with brackets, indicating 
-    the word represents a function. If we find 
-    brackets, we surround the word with 
-    backquotes, append it to a list, and use 
-    str.join() with " " as the seperator.
+    Check a string against a set of keywords, if the string exists, add 
+    backquotes, and append it to the list. If the word doesn't exist, just 
+    append to the list. 
 
     Args:
-    verify: str - the string we're splitting and 
-                  looping over.
-    
+        keywords : set[str] - the set containing our keywords.
+        verify : str - the word were checking for in our set.
+
     Returns:
-    str - the string with the function name 
-          (if any) properly quoted.
-          
-    Example:
-    Using title() becomes Using `title()`
+        Using .join(), we return our string properly quoted.
     """
-    return " ".join([f"`{word}`" if word.endswith("()") else word for word in verify.split(" ")])
+    return " ".join([f"`{word}`" if word in keywords else word for word in verify.split(" ")])
 
 def main():
-    
+
     try:
+        
+        keywords = set(get_keywords_from(file_name = "list.txt"))
 
         file_name = input("File: ")
         language = "python"
@@ -72,8 +82,8 @@ def main():
         # The section of code we jump to using the anchor.
         section : List[str] = []
 
-        with open(file_name, mode="r") as f_obj:
-           for anchor_with_spaces in f_obj:
+        with open(file_name, mode = "r") as f_obj:
+            for anchor_with_spaces in f_obj:
                 # If we're reading a .py file, the code is indented by 4 spaces,
                 # which we have to remove, so that we can use startsiwth(). We
                 # can use in, but this doesn't mean the # is at the beginning of the
@@ -82,11 +92,11 @@ def main():
                 if anchor.startswith("#") and anchor.endswith(":"):
                     # If we find the # at the start and : at the end, we slice it out.
                     anchor = anchor[2:][:-1]
-                    anchor = check_for_function_names(verify = anchor)
+                    anchor = convert_keywords_to_markdown_syntax(keywords = keywords, verify = anchor)
                     link = anchor.lower().replace(" ", "-")
                     anchors.append(f"- [{anchor}](#{link})")
                     section.append(f'#### <a name="{link}"></a> {anchor}:\n```{language}\n```')
-        
+
         # Using the comments from example.py, the .md file should be:
         """
             - [Using `print()`](#using-`print()`)
@@ -103,6 +113,6 @@ def main():
 
     except(ValueError, FileNotFoundError) as e:
         print(e)
-
+    
 if __name__ == "__main__":
     main()
